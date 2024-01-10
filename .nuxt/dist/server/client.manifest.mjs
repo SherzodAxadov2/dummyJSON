902728dca81af640501be792193bcfba198c9dca
Sherzod Axadov <101390847+SherzodAxadov2@users.noreply.github.com>
2024-01-10 06:29:01 +0000

    remove unnecessary fules

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/macuser/Desktop/uic/dummyJSON/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- .nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/macuser/Desktop/uic/dummyJSON/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
